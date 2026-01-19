11b5d2caea10e507c82c540dc4285c744deb4923
abdullah1610 <54662933+abdullah1610@users.noreply.github.com>
2026-01-19 14:24:52 +0530

    Update output values for VPC resources

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  value = aws_vpc.my_vpc.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  value = aws_subnet.public-subnet.id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+  value = aws_subnet.Private-subnet.id
 }
 
 output "security_group_id" {
-  value = aws_security_group.this.id
+  value = aws_security_group.my_sg.id
 }
